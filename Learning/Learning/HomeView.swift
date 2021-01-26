37db2688723a7fd8489933c12cbfee4234a2a90d
Dan Smtih <dan.smith@kinandcarta.com>
2021-01-26 20:53:55 +0000

    Adds simple UI and fetch predicate to interact with core data

---
+++ Learning/Learning/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  Learning
+//
+//  Created by Dan Smith on 26/01/2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+	@EnvironmentObject var dataController: DataController
+	
+	var body: some View {
+		NavigationView {
+			VStack {
+				Button("Add Data") {
+					dataController.deleteAll()
+					try? dataController.createSampleData()
+				}
+			}
+			.navigationTitle("Home")
+		}
+	}
+}
+
+struct HomeView_Previews: PreviewProvider {
+	static var previews: some View {
+		HomeView()
+	}
+}
